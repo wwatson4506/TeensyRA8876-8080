9d7d0b83f908857b28ed11e7633fee89b77cdab3
Warren Watson <wwatson4506@gmail.com>
2024-08-20 10:22:59 -0700

    Update RA8876_Config_8080.h

--- src/RA8876_Config_8080.h
+++ src/RA8876_Config_8080.h
@@ -9,6 +9,7 @@
 #define USE_8080_8_BIT_MODE 8
 #define USE_8080_16_BIT_MODE 16
 
+// 8080 control signal defines for T4/MicroMod/DB5 (W/O shield)
 #define RA8876_8080_CS 11
 #define RA8876_8080_DC 13
 #define RA8876_8080_RESET 12
@@ -17,11 +18,16 @@
 #define RA8876_RD 52
 #define RA8876_D0 40
 
-// DB5 Shield
-//#define RA8876_8080_CS 11
-//#define RA8876_8080_DC 13
-//#define RA8876_8080_RESET 12
-
+/*
+// Hardware defines DB5 board and Kurt's shield
+#define RA8876_8080_CS 53
+#define RA8876_8080_RESET 54
+#define RA8876_8080_DC 55
+#define RA8876_D0 40
+#define BACKLITE 29
+#define RA8876_RD 52    // FlexIO3:10: RD
+#define RA8876_WR 56    // FlexIO3:11 WR
+*/
 #define BUS_SPEED 20 //Available settings 2,4,8,12,20,24,30,40,60,120
 
 //External backlight control connected to this Arduino pin
