3e1546b2917e23675bdcd7b16c238712588784a0
Warren Watson <wwatson4506@gmail.com>
2024-08-22 11:26:28 -0700

    Update file.

--- src/RA8876_Config_8080.h
+++ src/RA8876_Config_8080.h
@@ -5,20 +5,24 @@
 #ifndef RA8876_CONFIG_H
 #define RA8876_CONFIG_H
 
-// Hardware defines
 #define USE_8080_8_BIT_MODE 8
 #define USE_8080_16_BIT_MODE 16
+#define BUS_SPEED 20 //Available settings 2,4,8,12,20,24,30,40,60,120
 
-// 8080 control signal defines for T4/MicroMod/DB5 (W/O shield)
+//External backlight control connected to this Arduino pin
+// Otherwise 3.3v
+//#define BACKLITE 5 or change to your pin choice
+
+#if defined(ARDUINO_TEENSY41)
+// Hardware defines T4.1
 #define RA8876_8080_CS 11
 #define RA8876_8080_DC 13
 #define RA8876_8080_RESET 12
+#define RA8876_D0 19
+#define RA8876_RD 37    // FlexIO3:10: RD
+#define RA8876_WR 36    // FlexIO3:11 WR
 
-#define RA8876_WR 53
-#define RA8876_RD 52
-#define RA8876_D0 40
-
-/*
+#elif defined(ARDUINO_TEENSY_DEVBRD5)
 // Hardware defines DB5 board and Kurt's shield
 #define RA8876_8080_CS 53
 #define RA8876_8080_RESET 54
@@ -27,12 +31,7 @@
 #define BACKLITE 29
 #define RA8876_RD 52    // FlexIO3:10: RD
 #define RA8876_WR 56    // FlexIO3:11 WR
-*/
-#define BUS_SPEED 20 //Available settings 2,4,8,12,20,24,30,40,60,120
-
-//External backlight control connected to this Arduino pin
-//#define BACKLITE 5 
-// #define BACKLITE 29 
+#endif
 
 // Uncomment to use FT5206 touch.
 #define USE_FT5206_TOUCH
